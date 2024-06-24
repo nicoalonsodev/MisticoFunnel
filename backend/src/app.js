629da5422025ef7a97d384e87e778ed1ea0f39e9
nicoalonsodev <160612968+nicoalonsodev@users.noreply.github.com>
2024-06-24 10:17:01 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {   //https://jeans.misticoar.com     http://localhost:3000  http://localhost:3001/   --omit=dev  https://misticofunnel-production.up.railway.app/
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://jeans.misticoar.com'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
